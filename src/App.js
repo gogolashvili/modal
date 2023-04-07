8f05dc70f9b1dc465aed6ac94aa213dc2a007587
gogolashvili <ggogolashvili91@gmail.com>
2023-04-07 18:25:10 +0400

    style: add desktop version styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Modal from "./Modal";
+import { useState } from "react";
 
 function App() {
-  return;
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <>
+      <div className="mainDiv">
+        <button className="mainBtn" onClick={() => setOpenModal(true)}>
+          MODAL
+        </button>
+        <Modal open={openModal} onClose={() => setOpenModal(false)} />
+      </div>
+    </>
+  );
 }
 
 export default App;
